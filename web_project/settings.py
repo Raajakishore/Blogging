98fe9ce39fae031d3a5372018da677c6e895997b
RAAJAKISHORE R <55315093+Raajakishore@users.noreply.github.com>
2020-12-22 16:47:41 +0530

    Update settings.py

--- web_project/settings.py
+++ web_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'be0idq3@!6%2%2%khpzk8u-y!jc4x$8%y#(ic-b26!!+z7lyt2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
